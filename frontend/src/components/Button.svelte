6cb97e1b5fbcf45b63a43fbe26180cba2b97550a
Joona Nevalainen <joona.nevalainen@iki.fi>
2023-10-14 21:53:39 +0300

    fix: fix disabled button colors

--- frontend/src/components/Button.svelte
+++ frontend/src/components/Button.svelte
@@ -9,18 +9,19 @@
 <button
     on:click|stopPropagation
     {type}
-    class="transition-all h-auto w-auto rounded-md p-1 bg-emerald-600 text-gray-50 flex flex-row items-center space-x-1"
-    class:bg-sky-500={color === 'blue'}
-    class:hover:bg-sky-400={color === 'blue'}
-    class:bg-emerald-500={color === 'green'}
-    class:hover:bg-emerald-400={color === 'green'}
-    class:bg-neutral-200={color === 'neutral'}
-    class:hover:bg-neutral-100={color === 'neutral'}
-    class:text-gray-800={color === 'neutral'}
+    class="transition-all h-auto w-auto rounded-md p-1 bg-emerald-60 flex flex-row items-center space-x-1"
+    class:bg-sky-500={!disabled && color === 'blue'}
+    class:hover:bg-sky-400={!disabled && color === 'blue'}
+    class:bg-emerald-500={!disabled && color === 'green'}
+    class:hover:bg-emerald-400={!disabled && color === 'green'}
+    class:bg-neutral-200={!disabled && color === 'neutral'}
+    class:hover:bg-neutral-100={!disabled && color === 'neutral'}
+    class:text-gray-800={!disabled && color === 'neutral'}
     class:aspect-square={square}
     class:px-4={!square}
     class:bg-gray-100={disabled}
     class:hover:bg-gray-100={disabled}
+    class:text-gray-50={!disabled}
     class:text-gray-400={disabled}
     class:italic={disabled}
     class:py-4={large}
