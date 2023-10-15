3df4cce91ec98d06def67d4199a7fbddb09e60c8
Joona Nevalainen <joona.nevalainen@phz.fi>
2023-10-15 19:54:11 +0300

    chore: dockerize everything

--- frontend/src/routes/tasks/[id]/+page.server.ts
+++ frontend/src/routes/tasks/[id]/+page.server.ts
@@ -1,9 +1,9 @@
-import { API_URL } from '$env/static/private';
+import { PUBLIC_API_URL } from '$env/static/public';
 import axios from 'axios';
 
 export async function load({ params }) {
     const id = await params.id;
-    const { data } = await axios.get(`${API_URL}/tasks/${id}`).catch((err) => {
+    const { data } = await axios.get(`${PUBLIC_API_URL}/tasks/${id}`).catch((err) => {
         console.error(err);
         return {};
     });
